302895d792db3a8b1abeef5699d72ff0faa81894
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-08-02 05:28:13 +0530

    bubble

--- Sorting/index-01.js
+++ Sorting/index-01.js
@@ -1,30 +1,34 @@
 
-function bubbleSort(arr, n) {
-    let i, j, temp, swapped;
+// function bubbleSort(arr, n) {
+//     let i, j, temp, swapped;
 
-    for (let i = 0; i < n - 1; i++) {
+//     for (let i = 0; i < n - 1; i++) {
 
-        swapped = false;
-        for (let j = 0; j < n - i - 1; j++) {
+//         swapped = false;
+//         for (let j = 0; j < n - i - 1; j++) {
 
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                swapped = true;
-            }
-        }
-        if (swapped == false) break;
-    }
-}
-function printArray(arr, size) {
-    let i;
-    for (let i = 0; i < size; i++) {
-        console.log(arr[i] + " ");
-    }
-}
-let arr = [10, 50, 30, 60, 20, 70, 80, 40];
-let n = arr.length;
-bubbleSort(arr, n);
-console.log("Sorted Array is : ");
-printArray(arr, n);
+//             if (arr[j] > arr[j + 1]) {
+//                 temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//                 swapped = true;
+//             }
+//         }
+//         if (swapped == false) break;
+//     }
+// }
+// function printArray(arr, size) {
+//     let i;
+//     for (let i = 0; i < size; i++) {
+//         console.log(arr[i] + " ");
+//     }
+// }
+// let arr = [10, 50, 30, 60, 20, 70, 80, 40];
+// let n = arr.length;
+// bubbleSort(arr, n);
+// console.log("Sorted Array is : ");
+// printArray(arr, n);
+
+
+
+
