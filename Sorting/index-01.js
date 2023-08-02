53a851eba102fa26a97cb4b4951b7efe7f403da9
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-08-02 07:35:16 +0530

    selection sort

--- Sorting/index-01.js
+++ Sorting/index-01.js
@@ -32,3 +32,34 @@
 
 
 
+function swap(arr, xp, yp) {
+    let temp = arr[xp];
+    arr[xp] = arr[yp];
+    arr[yp] = temp;
+}
+function selectionSort(arr, n) {
+    let i, j, min_idx;
+
+    for (let i = 0; i < n - 1; i++) {
+        min_idx = i;
+
+        for (let j = i + 1; j < n; j++) {
+
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+                swap(arr, min_idx, i);
+            }
+        }
+    }
+}
+function printArray(arr, size) {
+    let i;
+    for (let i = 0; i < size; i++) {
+        console.log(arr[i] + " ");
+    }
+}
+let arr = [10, 40, 46, 35, 40, 20];
+let n = arr.length;
+selectionSort(arr, n);
+console.log("Sorted array id : ");
+printArray(arr, n);
