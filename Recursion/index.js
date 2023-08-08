416d8e749272557326c4c7812c6236d305a17399
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-08-09 04:50:02 +0530

    revision

--- Recursion/index.js
+++ Recursion/index.js
@@ -1,13 +1,32 @@
-function printFun(test) {
+// function printFun(test) {
 
-    if (test < 1) return 1;
+//     if (test < 1) return 1;
+
+//     else {
+//         console.log(test + " ");
+//         printFun(test - 1);
+//         console.log(test + " ");
+//         return;
+//     }
+// }
+// let test = 4;
+// printFun(test);
+
+
+
+// Fibnacci number find 
+
+function findFibnacciNum(n) {
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
 
     else {
-        console.log(test + " ");
-        printFun(test - 1);
-        console.log(test + " ");
-        return;
+        return findFibnacciNum(n - 1) + findFibnacciNum(n - 2);
     }
 }
-let test = 4;
-printFun(test);
+let n = 8;
+console.log("Fibnacci number is : ");
+
+for (let i = 0; i < n; i++) {
+    console.log(findFibnacciNum(i) + " ");
+}
